d6e71d0463af4f0f8d057203562fb2eb8cb9bde8
COMPUTIX <office@computix.at>
2015-03-05 21:58:47 +0100

    Added function ask_user(prompt).

--- computix.py
+++ computix.py
@@ -73,7 +73,7 @@ def up_to_vowel(s):
     'My n'
     >>> up_to_vowel('Xyz')
     'Xyz'
-    >>> up_to_vowel('I like bread.')
+    >>> kjup_to_vowel('I like bread.')
     ''
     '''
 
@@ -85,3 +85,19 @@ def up_to_vowel(s):
         i = i + 1
 
     return string
+
+def ask_user(prompt):
+    '''(str) -> str
+
+    Use prompt to ask the user for a "yes" or "no"
+    answer and continue asking until the user gives
+    a valid response. Return the answer.
+    '''
+    
+    answer = input(prompt)
+
+    while not (answer == 'yes' or answer == 'Yes' or answer  == 'no' or answer == 'No'):
+        answer = input(prompt)
+
+    return answer
+        
