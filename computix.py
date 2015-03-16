3937a126b23b57bc977b1f898e656cce9c47937c
COMPUTIX <office@computix.at>
2015-03-16 21:26:41 +0100

    Add function count_matches(s1, s2).

--- computix.py
+++ computix.py
@@ -165,3 +165,29 @@ def sum_numbers(list1, list2):
         new_list.append(summe)
 
     return new_list
+
+def count_matches(s1, s2):
+    '''(str, str) -> int
+
+    Return the number of positions in s1 that contain
+    the same character at the corresponding position of s2.
+
+    Precondition: len(s1) == len(s2)
+
+    >>> count_matches('ate', 'ape')
+    2
+    >>> count_matches('head', 'hard')
+    2
+    >>> count_matches('nope', 'pope')
+    3
+    >>> count_matches('Hello', 'Going')
+    0
+    '''
+
+    num_matches = 0
+
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            num_matches = num_matches + 1
+
+    return num_matches
